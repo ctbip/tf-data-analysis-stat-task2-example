031f7f02289bdc0caf3899284f37ba344740f3c4
Daniil <50438498+ctbip@users.noreply.github.com>
2023-04-07 02:55:20 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,19 +1,18 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import expon
+from scipy.stats import gamma
 
 
-chat_id = 784892881 # Ваш chat ID, не меняйте название переменной
+chat_id = 784892881
 
 def solution(p: float, x: np.array) -> tuple:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+    
     alpha = 1 - p
-    min_val = np.min(x*x)
+    sum_r2 = np.sum(x*x)
     N = len(x)
-    # return np.sqrt(N*min_val/expon.ppf(1 - alpha/2)/30), np.sqrt(N*min_val/expon.ppf(alpha/2)/30)
-    # return 0, np.sqrt(N*min_val/expon.ppf(alpha)/30)
-    # return np.sqrt(N*min_val/expon.ppf(1 - alpha)/30), np.sqrt(N*min_val/expon.ppf(1)/30)
-    return np.sqrt(2)-0.1, np.sqrt(2)+0.1
+    
+    left_q = gamma.ppf(alpha/2, a=N, scale=2/N)
+    right_q = gamma.ppf(1-alpha/2, a=N, scale=2/N)
+
+    return np.sqrt(sum_r2/15/N/right_q), np.sqrt(sum_r2/15/N/left_q)
